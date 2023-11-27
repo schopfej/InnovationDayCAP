f512d14eab32b8e0ffe6258f5ffa13914397b607
Jonathan Schopfer <jonathan.schopfer@novo-bc.ch>
2023-11-27 16:56:37 +0000

    initial CAP Service

--- app/innoappmodule2/dist/controller/App.controller.js
+++
@@ -1,1 +0,0 @@
-sap.ui.define(["./BaseController"],function(n){"use strict";return n.extend("ch.novobc.innoappmodule2.controller.App",{onInit:function(){this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass())}})});
